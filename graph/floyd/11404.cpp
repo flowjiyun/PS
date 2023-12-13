6d9808dae912554c1dfe2a3345229bd8986e71d1
flowjiyun <gyp2795@gmail.com>
2023-12-13 16:18:01 +0900

    feat: floyd
    - 가중치가 있는 그래프에서 간선의 가중치가 있을 때 모든 노드간의 최적 구하기

---
+++ graph/floyd/11404.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+int n, m;
+int map[101][101];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i != j) {
+                map[i][j] = 987654321;
+            }
+        }
+    }
+    for (int i = 0; i < m; ++i) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        map[a][b] = min(map[a][b], c);
+    }
+    for (int m = 1; m <= n; ++m) {
+        for (int s = 1; s <= n; ++s) {
+            for (int e = 1; e <= n; ++e) {
+                if (map[s][e] > map[s][m] + map[m][e]) {
+                    map[s][e] = map[s][m] + map[m][e];
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (map[i][j] == 987654321) {
+                cout << "0" << ' ';
+            } else {
+                cout << map[i][j] << ' ';
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
