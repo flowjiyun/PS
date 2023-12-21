1f0ad7c8a1074c88b16dc5196a4f6c2dde2c5310
flowjiyun <gyp2795@gmail.com>
2023-12-21 10:29:03 +0900

    feat: folyd
    - 그래프의 모든 노드에서 특정 노드까지의 최단거리를 찾는 알고 리즘
    - 시간 복잡도 : O(n^3)

--- graph/floyd/11404.cpp
+++ graph/floyd/11404.cpp
@@ -4,41 +4,58 @@
 #include <string>
 #include <algorithm>
 
+// 시간 복잡도 o(n^3)
+// 모든 노드에서 특정 노드 까지의 최단거리를 찾을 수 있다
+
 using namespace std;
 int n, m;
 int map[101][101];
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> n >> m;
 
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (i != j) {
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            if (i != j)
+            {
                 map[i][j] = 987654321;
             }
         }
     }
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i)
+    {
         int a, b, c;
         cin >> a >> b >> c;
         map[a][b] = min(map[a][b], c);
     }
-    for (int m = 1; m <= n; ++m) {
-        for (int s = 1; s <= n; ++s) {
-            for (int e = 1; e <= n; ++e) {
-                if (map[s][e] > map[s][m] + map[m][e]) {
+    for (int m = 1; m <= n; ++m)
+    {
+        for (int s = 1; s <= n; ++s)
+        {
+            for (int e = 1; e <= n; ++e)
+            {
+                if (map[s][e] > map[s][m] + map[m][e])
+                {
                     map[s][e] = map[s][m] + map[m][e];
                 }
             }
         }
     }
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (map[i][j] == 987654321) {
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            if (map[i][j] == 987654321)
+            {
                 cout << "0" << ' ';
-            } else {
+            }
+            else
+            {
                 cout << map[i][j] << ' ';
             }
         }
