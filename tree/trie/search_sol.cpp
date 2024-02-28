2677090d81760a2f28a7b78b500448884667cf58
flowjiyun <gyp2795@gmail.com>
2024-02-28 10:23:32 +0900

    feat : 검색 시스템 문제 (trie)

---
+++ tree/trie/search_sol.cpp
@@ -0,0 +1,17 @@
+struct Result {
+  int mOrder;
+  int mRank;
+};
+
+void init() { return; }
+
+void search(char mStr[], int mCount) { return; }
+
+Result recommend(char mStr[]) {
+  Result ret = {0, 0};
+  return ret;
+}
+
+int relate(char mStr1[], char mStr2[]) { return 0; }
+
+void rank(char mPrefix[], int mRank, char mReturnStr[]) { return; }
