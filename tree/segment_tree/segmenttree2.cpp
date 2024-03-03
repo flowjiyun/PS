725f2aef0fbea118ce09350749f78a86f93a139d
flowjiyun <gyp2795@gmail.com>
2024-03-03 18:43:33 +0900

    fix : segment tree integer overflow 해결

--- tree/segment_tree/segmenttree2.cpp
+++ tree/segment_tree/segmenttree2.cpp
@@ -3,30 +3,31 @@
 #include <iostream>
 #include <limits>
 #include <vector>
+#define ll long long
 using namespace std;
 
 struct SegmentTree {
 
-  SegmentTree(int n, vector<int> &data, function<int(int, int)> merge, int def)
+  SegmentTree(int n, vector<ll> &data, function<ll(ll, ll)> merge, ll def)
       : mData(data), mMerge(merge), mDefault(def) {
     mTreeSize = n;
     mTree.resize(4 * n);
     makeTree(1, 0, mTreeSize - 1);
   }
 
-  int makeTree(int nodeID, int left, int right) {
+  ll makeTree(int nodeID, int left, int right) {
     // leaf node
     if (left == right) {
       return mTree[nodeID] = mData[left];
     }
     int mid = left + (right - left) / 2;
-    int leftChild = makeTree(2 * nodeID, left, mid);
-    int rightChild = makeTree(2 * nodeID + 1, mid + 1, right);
+    ll leftChild = makeTree(2 * nodeID, left, mid);
+    ll rightChild = makeTree(2 * nodeID + 1, mid + 1, right);
     return mTree[nodeID] = mMerge(leftChild, rightChild);
   }
 
   // update query (point update)
-  int updateTree(int index, int newVal, int nodeID, int left, int right) {
+  ll updateTree(int index, int newVal, int nodeID, int left, int right) {
     // out of range
     if (index < left || right < index) {
       return mTree[nodeID];
@@ -37,15 +38,15 @@ struct SegmentTree {
     }
 
     int mid = left + (right - left) / 2;
-    int leftChild = updateTree(index, newVal, 2 * nodeID, left, mid);
-    int rightChild = updateTree(index, newVal, 2 * nodeID + 1, mid + 1, right);
+    ll leftChild = updateTree(index, newVal, 2 * nodeID, left, mid);
+    ll rightChild = updateTree(index, newVal, 2 * nodeID + 1, mid + 1, right);
     return mTree[nodeID] = mMerge(leftChild, rightChild);
   }
 
   // range query
   // start, end = > query range
   // left, right => tree range
-  int query(int start, int end, int nodeID, int left, int right) {
+  ll query(int start, int end, int nodeID, int left, int right) {
     if (end < left || right < start) {
       return mDefault;
     }
@@ -54,8 +55,8 @@ struct SegmentTree {
     }
 
     int mid = left + (right - left) / 2;
-    int leftChild = query(start, end, 2 * nodeID, left, mid);
-    int rightChild = query(start, end, 2 * nodeID + 1, mid + 1, right);
+    ll leftChild = query(start, end, 2 * nodeID, left, mid);
+    ll rightChild = query(start, end, 2 * nodeID + 1, mid + 1, right);
     return mMerge(leftChild, rightChild);
   }
 
@@ -67,10 +68,10 @@ struct SegmentTree {
   }
 
   int mTreeSize;
-  vector<int> mTree;
-  vector<int> mData;
-  function<int(int, int)> mMerge;
-  int mDefault;
+  vector<ll> mTree;
+  vector<ll> mData;
+  function<ll(ll, ll)> mMerge;
+  ll mDefault;
 };
 
 int main(int argc, char **argv) {
@@ -82,14 +83,14 @@ int main(int argc, char **argv) {
   cin >> T;
   for (test_case = 1; test_case <= T; ++test_case) {
     int n, q, halfN;
-    vector<int> evenData;
-    vector<int> oddData;
+    vector<ll> evenData;
+    vector<ll> oddData;
     cin >> n >> q;
 
     if (n % 2 == 0) {
-      halfN = n / 2;
+      halfN = n >> 1;
     } else {
-      halfN = (n + 1) / 2;
+      halfN = (n + 1) >> 1;
     }
 
     // index 기준 짝수 및 홀수 데이터 나눠서 저장해서 segment tree 만들기
@@ -97,16 +98,16 @@ int main(int argc, char **argv) {
     oddData.resize(halfN);
     for (int i = 0; i < n; ++i) {
       if (i % 2 == 0) {
-        cin >> evenData[i / 2];
+        cin >> evenData[i >> 1];
       } else {
-        cin >> oddData[i / 2];
+        cin >> oddData[i >> 1];
       }
     }
 
     SegmentTree evenTree(
-        halfN, evenData, [](int a, int b) { return a + b; }, 0);
+        halfN, evenData, [](ll a, ll b) { return a + b; }, 0);
     SegmentTree oddTree(
-        halfN, oddData, [](int a, int b) { return a + b; }, 0);
+        halfN, oddData, [](ll a, ll b) { return a + b; }, 0);
 
     cout << "#" << test_case << " ";
     for (int i = 0; i < q; ++i) {
@@ -114,22 +115,22 @@ int main(int argc, char **argv) {
       cin >> command >> a >> b;
       if (command == 0) {
         if (a % 2 == 0) {
-          evenTree.updateTree(a / 2, b, 1, 0, halfN - 1);
+          evenTree.updateTree(a >> 1, b, 1, 0, halfN - 1);
         } else {
-          oddTree.updateTree(a / 2, b, 1, 0, halfN - 1);
+          oddTree.updateTree(a >> 1, b, 1, 0, halfN - 1);
         }
       } else {
         // 쿼리 시작 인덱스가 짝수, 홀수 경우의 수 나누기
         // 쿼리 사이즈가 1개 일경우 1개 이상일 경우
         int qSize = b - a;
-        int halfQSize = qSize / 2;
+        int halfQSize = qSize >> 1;
         int evenStartIndex;
         int oddStartIndex;
         int evenEndIndex;
         int oddEndIndex;
-        int ret;
+        ll ret;
         if (a % 2 == 0) {
-          evenStartIndex = a / 2;
+          evenStartIndex = a >> 1;
           oddStartIndex = evenStartIndex;
           if (qSize % 2 == 0) {
             evenEndIndex = evenStartIndex + halfQSize - 1;
@@ -141,7 +142,7 @@ int main(int argc, char **argv) {
           ret = evenTree.query(evenStartIndex, evenEndIndex, 1, 0, halfN - 1) -
                 oddTree.query(oddStartIndex, oddEndIndex, 1, 0, halfN - 1);
         } else {
-          oddStartIndex = a / 2;
+          oddStartIndex = a >> 1;
           evenStartIndex = oddStartIndex + 1;
           if (qSize % 2 == 0) {
             oddEndIndex = oddStartIndex + halfQSize - 1;
